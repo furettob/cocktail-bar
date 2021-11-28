2cb6b71b1f19415f4e572ffa1cf96eef92e1d5d4
spensieri-s <stefano.spensieri@docity.com>
2021-11-28 19:38:56 +0100

    Extracted getIngredients function + added getDrinkById function

--- src/utils/dataHub.js
+++ src/utils/dataHub.js
@@ -13,9 +13,8 @@ export async function getCocktails(searchStr) {
 }
 
 export async function getRandomCocktail() {
-    console.log("asking")
+    console.log("getRandomCocktail")
     const resp = await Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    console.log("received")
     if (!resp?.data || resp.data === "") {
         console.log("ERROR::::: ")
         return {error: resp}
@@ -23,4 +22,30 @@ export async function getRandomCocktail() {
 
     console.log("RANDOM COCKTAIL DATA:::: ", resp)
     return resp.data.drinks
+}
+
+export async function getDrinkById(id) {
+    console.log("getDrinkById ", id)
+    const resp = await Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    if (!resp?.data || resp.data === "") {
+        console.log("ERROR::::: ")
+        return {error: resp}
+    }
+
+    console.log("DRINK DATA:::: ", resp.data.drinks[0])
+    return resp.data.drinks[0]
+}
+
+
+export function getIngredients(drink) {
+    const ingredients = Array.from(Array(25).keys()).map((elem, index) => {
+            if (drink["strIngredient" + index]) {
+                return {
+                    "ingredient": drink["strIngredient" + index],
+                    "measure": drink["strMeasure" + index] ? drink["strMeasure" + index].trim() : null
+                }
+            }
+        }
+    ).filter(elem => !!elem)
+    return ingredients
 }
