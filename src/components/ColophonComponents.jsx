3b8a39d9656b859c11a4c8df297236b4714fbc99
spensieri-s <stefano.spensieri@docity.com>
2021-11-30 22:58:39 +0100

    Externalized authors json

--- src/components/ColophonComponents.jsx
+++ src/components/ColophonComponents.jsx
@@ -2,48 +2,9 @@ import * as React from 'react'
 import 'font-awesome/css/font-awesome.min.css'
 import UserCard from './UserCard'
 import Row from './Row'
+import authors from '../utils/StaticData/authors.json'
 
 function ColophonComponents() {
-
-    const authors = [
-        {
-            name: "Marco",
-            lastname: "Corradetti",
-            jobPosition: "Frontend developer",
-            company: "Docsity",
-            avatarUrl: "/img/marco_avatar.png",
-            role: "Author",
-            links: [
-                {name: "Github", logo: "fa fa-github", url:"https://github.com/corradetti-s"},
-                {name: "Linkedin", logo: "fa fa-linkedin", url:"https://www.linkedin.com/in/sspensieri/"},
-                {name: "Docsity", logo: "fa fa-globe", url:"https://www.docsity.com"},
-            ]
-        },
-        {
-            name: "Stefano",
-            lastname: "Spensieri",
-            jobPosition: "Frontend developer",
-            company: "Docsity",
-            avatarUrl: "/img/stefano_avatar.jpeg",
-            role: "Author",
-            links: [
-                {name: "Github", logo: "fa fa-github", url:"https://github.com/spensieri-s"},
-                {name: "Linkedin", logo: "fa fa-linkedin", url:"https://www.linkedin.com/in/sspensieri/"},
-                {name: "Docsity", logo: "fa fa-globe", url:"https://www.docsity.com"},
-            ]
-        },
-        {
-            name: "Computer Science Students",
-            lastname: null,
-            company: "Corso Interazione Uomo Macchina e Tecnologie Web",
-            avatarUrl: "/img/unito.png",
-            jobPosition: "Computer Science Students",
-            role: "Editors",
-            links: [
-                {name: "Università di Torino", logo: "fa fa-university", url:"https://www.unito.it/"}
-            ]
-        },
-    ]
 
     return (
       <div className={"cb-colophon"}>
