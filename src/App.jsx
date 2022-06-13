91fadccd62b0e5b8d20bf47f1582c285ebb6b70e
spensieri-s <stefano.spensieri@docity.com>
2022-06-13 23:22:37 +0200

    Removed Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom"
 import AllCocktails from "./pages/AllCocktails"
-import DrinkDetail from "./pages/DrinkDetail"
 import Colophon from "./components/ColophonComponents"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <div className="cb-content">
-          <Switch>
-            <Route
-              path="/drinks"
-              render={() => <AllCocktails favourite={false} key={0} />}
-            />
-            <Route path="/drink/:id" render={() => <DrinkDetail key={1} />} />
-            <Route render={() => <Redirect to="/drinks" />} />
-          </Switch>
+          <AllCocktails favourite={false} />
           <Colophon />
         </div>
-      </Router>
     </div>
   )
 }
