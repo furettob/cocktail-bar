adcab7400d9eae7f332b3618a017964416b00ca6
spensieri-s <stefano.spensieri@docity.com>
2022-07-05 05:12:17 +0200

    Minor adjustments

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,28 +1,28 @@
 import React, {useState, useEffect} from "react";
-import {app} from "../utils/firebase";
+import {app, auth} from "../utils/firebase";
 
 export const AuthContext = React.createContext({user: null});
 
 const useAuth = () => {
-  const [state, setState] = useState( () => {
+  const [authState, setAuthState] = useState( () => {
     const user = app.auth().currentUser
     return {inizializing: !user, user}
 
   })
 
   function onChange(user) {
-    console.log("The current user is: ", user)
-    setState({initializing: false, user})
+    // console.log("The current user is: ", user)
+    setAuthState({initializing: false, user})
   }
 
   useEffect(() => {
     // You pass in the function to call when the auth state changes
     // You get as answer a "pointer" to unsubscribe
-    const unsubscribe = app.auth().onAuthStateChanged(onChange); // TODO: cambia app.auth() con auth (oggetto)
+    const unsubscribe = auth.onAuthStateChanged(onChange);
     return () => unsubscribe();
   }, []);
 
-  return state // TODO::: rename to authState
+  return authState
 }
 
 
