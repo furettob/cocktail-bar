c5922ea9b232d441e0bbf9dd5d16a4a0db757a4d
spensieri-s <stefano.spensieri@docity.com>
2022-07-05 05:18:23 +0200

    New firebase auth config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnIM3kTbY4xAeeeSVBUdPmVozmBD9WkFA",
-  authDomain: "fb-cocktailbar.firebaseapp.com",
-  projectId: "fb-cocktailbar",
-  storageBucket: "fb-cocktailbar.appspot.com",
-  messagingSenderId: "911121121439",
-  appId: "1:911121121439:web:1634a668e922c1818e22aa"
+  apiKey: "AIzaSyD6aOHMxEzUW-9_xy7oReS_O8ikQ1rSYnU",
+  authDomain: "fb-cocktailbar-v2.firebaseapp.com",
+  databaseURL: "https://fb-cocktailbar-v2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fb-cocktailbar-v2",
+  storageBucket: "fb-cocktailbar-v2.appspot.com",
+  messagingSenderId: "65892001055",
+  appId: "1:65892001055:web:50ce964c3ba28827aa7c2a"
 };
 
 // Initialize Firebase
