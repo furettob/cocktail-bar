509afbbaf133ce31a4da3e13c04e92c6b5610aba
spensieri-s <stefano.spensieri@docity.com>
2022-07-19 14:00:15 +0200

    Deleted console.log

--- web/src/hocs/withToggle.jsx
+++ web/src/hocs/withToggle.jsx
@@ -3,7 +3,6 @@
 import React, {Component} from 'react';
 
 export default function Hoc(HocComponent, data, clickCallback){
-  console.log("Rendering with ", clickCallback)
 
   return class extends Component{
     constructor(props) {
