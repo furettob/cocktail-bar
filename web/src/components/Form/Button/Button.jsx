d96db0873886f8e9608f5bf9a92d47f220837490
spensieri-s <stefano.spensieri@docity.com>
2022-07-19 13:59:40 +0200

    Styling the form, adding components

--- web/src/components/Form/Button/Button.jsx
+++ web/src/components/Form/Button/Button.jsx
@@ -1,8 +1,22 @@
 import * as React from "react"
 
 function Button(props) {
+  const getClass = () => {
+    let c = "cb-button"
+    let b = "cb-button"
+    if (props.fluid) {
+      c += " "+b+"--fluid"
+    }
+    if (props.inverted) {
+      c += " "+b+"--inverted"
+    }
+    if (props.pill) {
+      c += " "+b+"--pill"
+    }
+    return c
+  }
   return (
-    <span {...props} className={"cb-button"}>{props.label}</span>
+    <span {...props} className={getClass()}>{props.label}</span>
   )
 }
 
